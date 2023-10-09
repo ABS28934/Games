c50db03a9e76631a56038472902ea6cdf9c8e3e4
Anandi Nambiar <annambi@student.unimelb.edu.au>
2023-10-09 13:52:19 +1100

    end screen implemented

--- src/Lane.java
+++ src/Lane.java
@@ -1,58 +1,58 @@
-//import bagel.*;
-//
-//import java.security.Key;
-//import java.util.ArrayList;
-//
-//public class Lane {
-//    private final static int Y_COORDINATE = 384;
-//    private final static int TARGET_HEIGHT = 657;
-//    private String laneType;
-//    private int xCoordinate;
-//    private Keys relevantKey;
-//    private Image laneImage;
-//    private final ArrayList<NormalNote> normalNotes = new ArrayList<>();
-//    private final ArrayList<HoldNote> holdNotes = new ArrayList<>();
-//
-//    public Lane(String laneType, int xCoordinate, Keys relevantKey, Image laneImage){
-//        this.laneType = laneType;
-//        this.xCoordinate = xCoordinate;
-//        this.relevantKey = relevantKey;
-//        this.laneImage = laneImage;
-//    }
-//    public String getLaneType() {
-//        return laneType;
-//    }
-//
-//    public int getxCoordinate() {
-//        return xCoordinate;
-//    }
-//
-//    public static Keys getRelevantKey(String laneType){
-//        if (laneType.equals("Left")) {
-//            return Keys.LEFT;
-//        }
-//        if (laneType.equals("Right")) {
-//            return Keys.RIGHT;
-//        }
-//        if (laneType.equals("Up")) {
-//            return Keys.UP;
-//        }
-//        if (laneType.equals("Down")) {
-//            return Keys.DOWN;
-//        }
-//        if (laneType.equals("Special")) {
-//            return Keys.SPACE;
-//        }
-//        return null;
-//    }
-//
-//    public static Image getLaneImage(String laneType) {
-//            return new Image("res/lane" + laneType + ".png");
-//    }
-//
-//    public void drawLane() {
-//        laneImage.draw(xCoordinate, Y_COORDINATE);
-//    }
-//
-//
-//}
+import bagel.*;
+
+import java.security.Key;
+import java.util.ArrayList;
+
+public class Lane {
+    private final static int Y_COORDINATE = 384;
+    private final static int TARGET_HEIGHT = 657;
+    private String laneType;
+    private int xCoordinate;
+    private Keys relevantKey;
+    private Image laneImage;
+    private final ArrayList<NormalNote> normalNotes = new ArrayList<>();
+    private final ArrayList<HoldNote> holdNotes = new ArrayList<>();
+
+    public Lane(String laneType, int xCoordinate, Keys relevantKey, Image laneImage){
+        this.laneType = laneType;
+        this.xCoordinate = xCoordinate;
+        this.relevantKey = relevantKey;
+        this.laneImage = laneImage;
+    }
+    public String getLaneType() {
+        return laneType;
+    }
+
+    public int getxCoordinate() {
+        return xCoordinate;
+    }
+
+    public static Keys getRelevantKey(String laneType){
+        if (laneType.equals("Left")) {
+            return Keys.LEFT;
+        }
+        if (laneType.equals("Right")) {
+            return Keys.RIGHT;
+        }
+        if (laneType.equals("Up")) {
+            return Keys.UP;
+        }
+        if (laneType.equals("Down")) {
+            return Keys.DOWN;
+        }
+        if (laneType.equals("Special")) {
+            return Keys.SPACE;
+        }
+        return null;
+    }
+
+    public static Image getLaneImage(String laneType) {
+            return new Image("res/lane" + laneType + ".png");
+    }
+
+    public void drawLane() {
+        laneImage.draw(xCoordinate, Y_COORDINATE);
+    }
+
+
+}
--- src/Levels.java
+++ src/Levels.java
@@ -1,70 +1,70 @@
-//import java.io.BufferedReader;
-//import java.io.FileReader;
-//import bagel.*;
-//
-//public abstract class Levels {
-//    private int levelScore;
-//    private int targetScore;
-//    private String csvFile;
-//    private int laneNum = 0;
-//    private boolean finished;
-//
-//    private final NormalLane[] normalLanes = new NormalLane[4];
-//
-//    public void readCsv() {
-//        try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
-//            String textRead;
-//            while ((textRead = br.readLine()) != null) {
-//                String[] splitText = textRead.split(",");
-//
-//                if (splitText[0].equals("Lane") && !splitText[1].equals("Special")) {
-//                    // reading lanes
-//                    String laneType = splitText[1];
-//                    int pos = Integer.parseInt(splitText[2]);
-//                    Keys relevantKey = Lane.getRelevantKey(laneType);
-//                    Image laneImage = Lane.getLaneImage(laneType);
-//                    NormalLane normalLane = new NormalLane(laneType, pos, relevantKey,laneImage);
-//                    normalLanes[laneNum++] = normalLane;
-//                } else if (!splitText[0].equals("Lane") && !splitText[1].equals("Special")){
-//                    // reading notes
-//                    String dir = splitText[0];
-//                    NormalLane normalLane = null;
-//                    for (int i = 0; i < laneNum; i++) {
-//                        if (normalLanes[i].getLaneType().equals(dir)) {
-//                            normalLane = normalLanes[i];
-//                        }
-//                    }
-//
-//                    if (normalLane != null) {
-//                        switch (splitText[1]) {
-//                            case "Normal":
-//                                NormalNote note = new Note(dir, Integer.parseInt(splitText[2]));
-//                                normalLane.addNote(note);
-//                                break;
-//                            case "Hold":
-//                                HoldNote holdNote = new HoldNote(dir, Integer.parseInt(splitText[2]));
-//                                normallane.addHoldNote(holdNote);
-//                                break;
-//                        }
-//                    }
-//
-//                }
-//            }
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//            System.exit(-1);
-//        }
-//
-//    }
-//
-//    public boolean checkFinished() {
-//        for (int i = 0; i < laneNum; i++) {
-//            if (normalLanes[i].isNormalFinished()==true){
-//                finished = true;
-//            }
-//        }
-//        return finished;
-//    }
-//
-//
-//}
+import java.io.BufferedReader;
+import java.io.FileReader;
+import bagel.*;
+
+public abstract class Levels {
+    private int levelScore;
+    private int targetScore;
+    private String csvFile;
+    private int laneNum = 0;
+    private boolean finished;
+
+    private final NormalLane[] normalLanes = new NormalLane[4];
+
+    public void readCsv() {
+        try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
+            String textRead;
+            while ((textRead = br.readLine()) != null) {
+                String[] splitText = textRead.split(",");
+
+                if (splitText[0].equals("Lane") && !splitText[1].equals("Special")) {
+                    // reading lanes
+                    String laneType = splitText[1];
+                    int pos = Integer.parseInt(splitText[2]);
+                    Keys relevantKey = Lane.getRelevantKey(laneType);
+                    Image laneImage = Lane.getLaneImage(laneType);
+                    NormalLane normalLane = new NormalLane(laneType, pos, relevantKey,laneImage);
+                    normalLanes[laneNum++] = normalLane;
+                } else if (!splitText[0].equals("Lane") && !splitText[1].equals("Special")){
+                    // reading notes
+                    String dir = splitText[0];
+                    NormalLane normalLane = null;
+                    for (int i = 0; i < laneNum; i++) {
+                        if (normalLanes[i].getLaneType().equals(dir)) {
+                            normalLane = normalLanes[i];
+                        }
+                    }
+
+                    if (normalLane != null) {
+                        switch (splitText[1]) {
+                            case "Normal":
+                                NormalNote note = new Note(dir, Integer.parseInt(splitText[2]));
+                                normalLane.addNote(note);
+                                break;
+                            case "Hold":
+                                HoldNote holdNote = new HoldNote(dir, Integer.parseInt(splitText[2]));
+                                normallane.addHoldNote(holdNote);
+                                break;
+                        }
+                    }
+
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.exit(-1);
+        }
+
+    }
+
+    public boolean checkFinished() {
+        for (int i = 0; i < laneNum; i++) {
+            if (normalLanes[i].isNormalFinished()==true){
+                finished = true;
+            }
+        }
+        return finished;
+    }
+
+
+}
